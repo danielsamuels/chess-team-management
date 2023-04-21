897d1db7102cc1c6961e75a02c5e560d1c9f946d
Daniel Finch <daniel.samuels1@gmail.com>
2023-04-21 18:22:38 +0100

    Add a more general page layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Head, Html, Main, NextScript} from 'next/document'
+import {AppBar, Container, Toolbar, Typography} from "@mui/material";
+import Link from "next/link";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head/>
+            <body>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        className="mx-auto text-lg font-bold"
+                        href="/"
+                        component={Link}
+                    >
+                        Composition
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth="md" className="mt-4">
+                <Main/>
+            </Container>
+            <NextScript/>
+            </body>
+        </Html>
+    )
 }
